814b3999cc9a1acb15f1687350c3e71e173dc438
Ikramico <67937289+Ikramico@users.noreply.github.com>
2022-11-25 12:29:18 +0600

    Menu
    Creating Menu. Style will be done with Sass. This is just structure

---
+++ src/Components/menu.js
@@ -0,0 +1,19 @@
+<>
+<div className="row">
+    <div className="logo">
+        Appstrom
+    </div>
+    <div className="navbar">
+        <div className="nav-items">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">How it looks</li>
+            <li className="nav-item">About</li>
+            <li className="nav-item">Pricing</li>
+            <li className="nav-item">Gallery</li>
+            <li className="nav-item">Our clients</li>
+            <li className="nav-item">Contact</li>
+            <button className="nav-button">Download</button>
+        </div>
+    </div>
+</div>
+</>
